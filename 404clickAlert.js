423e2c2993f77ece487615aa1ce7a75f757acbc6
ytbg0512 <ytbg0512@icloud.com>
2021-01-17 22:35:07 +0900

    미역국

--- 404clickAlert.js
+++ 404clickAlert.js
@@ -5,12 +5,12 @@ let clickCount = 0;
 
 function runClickEvent() {
     if (clickCount == 5) {
-        body.style.backgroundImage = "url(https://cdn.pixabay.com/photo/2017/01/05/09/18/food-1954424_960_720.png)";
-        alert('김치찌개 모드 활성화됨');
+        body.style.backgroundImage = "url(https://www.costco.co.kr/medias/sys_master/images/h70/h5d/9868030345246.jpg)";
+        alert('미역국 모드 활성화됨');
     } else if (clickCount > 9) {
         body.style.backgroundImage = "none";
         clickCount = 0;
-        alert('김치찌개 모드 비활성화됨');
+        alert('미역국 모드 비활성화됨');
     }
 }
 
